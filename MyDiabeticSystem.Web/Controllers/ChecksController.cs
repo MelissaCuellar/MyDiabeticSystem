5a45c1f2bb9b8f0b3a042153c439339a17e951ad
MelissaCuellar <43379979+MelissaCuellar@users.noreply.github.com>
2019-10-04 12:51:47 -0500

    Add ChecksController and views.

---
+++ MyDiabeticSystem.Web/Controllers/ChecksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyDiabeticSystem.Web.Data;
+using MyDiabeticSystem.Web.Data.Entities;
+
+namespace MyDiabeticSystem.Web.Controllers
+{
+    public class ChecksController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ChecksController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Checks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Checks.ToListAsync());
+        }
+
+        // GET: Checks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var check = await _context.Checks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (check == null)
+            {
+                return NotFound();
+            }
+
+            return View(check);
+        }
+
+        // GET: Checks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Checks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Carbohydrates,Glucometry,Date,Bolus,Hb1")] Check check)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(check);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(check);
+        }
+
+        // GET: Checks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var check = await _context.Checks.FindAsync(id);
+            if (check == null)
+            {
+                return NotFound();
+            }
+            return View(check);
+        }
+
+        // POST: Checks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Carbohydrates,Glucometry,Date,Bolus,Hb1")] Check check)
+        {
+            if (id != check.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(check);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CheckExists(check.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(check);
+        }
+
+        // GET: Checks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var check = await _context.Checks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (check == null)
+            {
+                return NotFound();
+            }
+
+            return View(check);
+        }
+
+        // POST: Checks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var check = await _context.Checks.FindAsync(id);
+            _context.Checks.Remove(check);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CheckExists(int id)
+        {
+            return _context.Checks.Any(e => e.Id == id);
+        }
+    }
+}
